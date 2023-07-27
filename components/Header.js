2b94b4ae852826d13272502f4dc91fd1f7742ed5
derobbio <gustavoderobbio@hotmail.com>
2023-07-27 12:40:15 -0300

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -107,8 +107,8 @@ export default function Header ({ navBarTitle, fullWidth }) {
           <Link href="/" aria-label={BLOG.title}>
             <Image
               src={favicon}
-              width={24}
-              height={24}
+              width={27}
+              height={27}
               alt={BLOG.title}
               onError={() => setFavicon(true)}
             />
