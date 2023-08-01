18e048b67f02970e9f099f05694435aa70604c26
derobbio <gustavoderobbio@hotmail.com>
2023-08-01 18:06:28 -0300

    2 tentativa

--- components/Header.js
+++ components/Header.js
@@ -103,7 +103,7 @@ export default function Header ({ navBarTitle, fullWidth }) {
             className="fill-black dark:fill-white"
           />
         </svg>
-        <div className="flex items-center">
+        <div className="vertical">
           <Link href="/" aria-label={BLOG.title}>
             <Image
               src={favicon}
